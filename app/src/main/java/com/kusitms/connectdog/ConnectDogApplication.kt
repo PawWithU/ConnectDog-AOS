a3f010384b7b8f034f7ba235280196b7dcb22edb
Minju Kim <mindori1019@gmail.com>
2023-10-10 07:10:43 +0900

    feature: hilt android app 선언

--- app/src/main/java/com/kusitms/connectdog/ConnectDogApplication.kt
+++ app/src/main/java/com/kusitms/connectdog/ConnectDogApplication.kt
@@ -1,6 +1,7 @@
 package com.kusitms.connectdog
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
-// just commit test
+@HiltAndroidApp
 class ConnectDogApplication: Application()
