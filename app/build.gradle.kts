256b93d22ffd276481895e05de69e69b1fb59097
Minju Kim <mindori1019@gmail.com>
2023-10-10 04:35:39 +0900

    refactor: MainActivity app 모듈 -> feature:main 모듈로 이동시킴

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,12 +37,7 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -51,21 +46,10 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
+    implementation(project(":feature:main"))
+    implementation(project(":feature:home"))
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
--- feature/main/src/main/java/com/kusitms/connectdog/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/kusitms/connectdog/feature/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kusitms.connectdog
+package com.kusitms.connectdog.feature.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,13 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.kusitms.connectdog.ui.theme.ConnectDogTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ConnectDogTheme {
+            com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +39,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ConnectDogTheme {
+    com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme {
         Greeting("Android")
     }
 }
