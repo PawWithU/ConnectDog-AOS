fd184d7dda355d674cfe436245b0ee0ee50dd240
Minju Kim <mindori1019@gmail.com>
2023-10-10 06:55:38 +0900

    feature: interface 생성자 전달을 위해 구현과 인터페이스 bind 작업

---
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/di/DataModule.kt
@@ -0,0 +1,17 @@
+package com.kusitms.connectdog.core.data.di
+
+import com.kusitms.connectdog.core.data.repository.DefaultExampleRepository
+import com.kusitms.connectdog.core.data.repository.ExampleRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+internal abstract class DataModule {
+    @Binds
+    abstract fun bindsExampleRepository(
+        repository: DefaultExampleRepository
+    ): ExampleRepository
+}
