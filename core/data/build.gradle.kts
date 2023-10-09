8bd7dd01385752404ce54e5df0cbbca854f02c66
Minju Kim <mindori1019@gmail.com>
2023-10-10 06:34:19 +0900

    build: core:data network 통신을 위한 dependencies 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -40,6 +40,12 @@ dependencies {
 
     kapt(libs.hilt.compiler)
     implementation(libs.hilt.android)
+
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.datetime)
 }
 
 kapt {
