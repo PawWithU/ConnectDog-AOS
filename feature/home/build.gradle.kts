e7ef9f568062f3df3e50ed51083eb6f911ca75eb
Minju Kim <mindori1019@gmail.com>
2023-10-10 04:34:33 +0900

    fix: compose 관련 의존성 추가 (feature 모두 해줘야함 ㅜㅜ)

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -47,8 +47,14 @@ dependencies {
     implementation(project(":core:designsystem"))
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -48,4 +48,12 @@ dependencies {
     implementation(project(":core:model"))
     implementation(project(":core:designsystem"))
 
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 }
