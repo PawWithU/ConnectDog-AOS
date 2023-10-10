4101d74576234b165b0721f7893a6bf37bccf6f9
Minju Kim <mindori1019@gmail.com>
2023-10-10 15:20:36 +0900

    feat: HomeScreen UI 예시 구현

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/HomeScreen.kt
@@ -1,9 +1,16 @@
 package com.kusitms.connectdog.feature.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.collectLatest
@@ -22,11 +29,27 @@ internal fun HomeRoute(
         viewModel.errorFlow.collectLatest { throwable -> onShowErrorSnackBar(throwable) }
     }
 
-    HomeScreen()
+    HomeScreen(
+        padding = padding,
+        exampleUiState = exampleUiState,
+        onClick = onClick
+    )
 }
 
 @Composable
 private fun HomeScreen(
+    padding: PaddingValues,
+    exampleUiState: ExampleUiState,
+    onClick: () -> Unit
 ) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier.padding(padding)
+            .padding(horizontal = 8.dp)
+            .verticalScroll(scrollState)
+            .padding(bottom = 4.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
 
+    }
 }
