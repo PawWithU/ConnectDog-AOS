c87bb95f8288e8f278457c663e319439f1c21b87
Minju Kim <mindori1019@gmail.com>
2023-10-10 15:53:12 +0900

    build: kotlinx immutable, persistent collection 라이브러리 추가

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    implementation(libs.kotlinx.collection.imuutable)
 }
 
 kapt {
