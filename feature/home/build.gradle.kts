d3665a7c73e877cc350dc588c081d29d29904af2
Minju Kim <mindori1019@gmail.com>
2023-10-10 14:30:38 +0900

    build: feature:home 모듈에 navigation 의존성 추가

--- feature/home/build.gradle.kts
+++ feature/home/build.gradle.kts
@@ -51,18 +51,19 @@ dependencies {
     kapt(libs.hilt.compiler)
     implementation(libs.hilt.android)
 
+    implementation(libs.androidx.compose.navigation)
+    implementation(libs.hilt.navigation.compose)
+
+    implementation(libs.androidx.lifecycle.viewModelCompose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
 
 kapt {
