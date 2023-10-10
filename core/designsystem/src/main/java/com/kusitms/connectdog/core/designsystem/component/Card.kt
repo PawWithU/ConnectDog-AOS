bb55240a94e503b7e1403e6c6655b39a8f24d5ba
Minju Kim <mindori1019@gmail.com>
2023-10-10 15:21:07 +0900

    feat: 공통 component로 사용할 Card 컴포저블 desginsystem에서 생성

---
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
@@ -0,0 +1,25 @@
+package com.kusitms.connectdog.core.designsystem.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ConnectDogCard(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.surface,
+    content: @Composable () -> Unit
+) {
+    Surface(
+        modifier = modifier.fillMaxWidth(),
+        color = color,
+        shape = RoundedCornerShape(32.dp),
+        shadowElevation = 2.dp,
+        content = content,
+    )
+}
