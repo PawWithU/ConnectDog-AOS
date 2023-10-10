23f16540d0496b3a128f33e4fd35e849bf18fd16
Minju Kim <mindori1019@gmail.com>
2023-10-10 15:00:32 +0900

    feat: feature:home에 홈화면 이동 로직 구현

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/HomeScreen.kt
@@ -1,6 +1,29 @@
 package com.kusitms.connectdog.feature.home
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.collectLatest
+
+@Composable
+internal fun HomeRoute(
+    padding: PaddingValues,
+    onClick: () -> Unit,
+    onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
+    viewModel: HomeViewModel = hiltViewModel()
+) {
+    val exampleUiState by viewModel.exampleUiState.collectAsStateWithLifecycle()
+
+    // 에러 발생할 때마다 에러 스낵바 표시
+    LaunchedEffect(true) {
+        viewModel.errorFlow.collectLatest { throwable -> onShowErrorSnackBar(throwable) }
+    }
+
+    HomeScreen()
+}
 
 @Composable
 private fun HomeScreen(
