7746530f7e306392098e2336831ce35aa7cfc413
Minju Kim <mindori1019@gmail.com>
2023-10-13 01:10:35 +0900

    update: ktlint formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.org.jetbrains.kotlin.android)
     kotlin("kapt")
     alias(libs.plugins.hilt)
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
 }
 
 android {
--- app/src/main/java/com/kusitms/connectdog/ConnectDogApplication.kt
+++ app/src/main/java/com/kusitms/connectdog/ConnectDogApplication.kt
@@ -4,4 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ConnectDogApplication: Application()
+class ConnectDogApplication : Application()
