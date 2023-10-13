3b72715b81718d056fe78bb377235254f18de312
SeungGu Kang <alex9366@naver.com>
2023-10-14 03:32:25 +0900

    [add] ktlint 테스트

--- feature/main/src/main/java/com/kusitms/connectdog/feature/main/Test.kt
+++ feature/main/src/main/java/com/kusitms/connectdog/feature/main/Test.kt
@@ -1,7 +1,7 @@
 package com.kusitms.connectdog.feature.main
 
 class Test {
-    val test: String = "ktlint 테스트 입니다"
+    val test : String = "ktlint 테스트 입니다"
 
     fun ktlintTest(){
         print(test)
