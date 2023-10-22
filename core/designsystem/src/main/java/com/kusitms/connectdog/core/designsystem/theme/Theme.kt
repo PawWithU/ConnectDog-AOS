b71f883ca553e03eeefe52e119b75f82b9d28f99
Minju Kim <mindori1019@gmail.com>
2023-10-22 22:35:34 +0900

    feature/style : designsystem 색깔, lightColorScheme 설정

--- core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -25,16 +26,15 @@ private val DarkColorScheme =
 
 private val LightColorScheme =
     lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
+        primary = PetOrange,
+        onPrimary = Color.White,
+        background = Color.White,
+        onBackground = Color.Black,
+        primaryContainer = Orange_container
     /* Other default colors to override
-    background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
      */
     )
