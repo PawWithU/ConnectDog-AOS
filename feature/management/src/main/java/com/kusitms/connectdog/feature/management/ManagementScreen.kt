da084d0b07ea1b6ceb18264a6130a2f2ecf6eb11
Minju Kim <mindori1019@gmail.com>
2023-10-30 14:52:00 +0900

    feature/management : management navGraphBuilder 이동시 컴포넌트 구현

---
+++ feature/management/src/main/java/com/kusitms/connectdog/feature/management/ManagementScreen.kt
@@ -0,0 +1,26 @@
+package com.kusitms.connectdog.feature.management
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.kusitms.connectdog.feature.management.navigation.ManagementRoute
+
+@Composable
+internal fun ManagementRoute(
+    padding: PaddingValues,
+    onClick: () -> Unit,
+    onShowErrorSnackBar: (throwable: Throwable?) -> Unit
+) {
+    ManagementScreen(
+        padding = padding,
+        onClick = onClick
+    )
+}
+
+@Composable
+private fun ManagementScreen(
+    padding: PaddingValues,
+    onClick: () -> Unit
+) {
+    Text(text = "MANAGEMENT")
+}
