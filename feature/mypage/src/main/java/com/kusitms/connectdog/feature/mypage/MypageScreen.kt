44157d24caf14caeb8679d10b659e34f147eef21
Minju Kim <mindori1019@gmail.com>
2023-10-30 14:53:50 +0900

    feature/mypage : mypage navGraphBuilder 이동시 컴포넌트 구

---
+++ feature/mypage/src/main/java/com/kusitms/connectdog/feature/mypage/MypageScreen.kt
@@ -0,0 +1,25 @@
+package com.kusitms.connectdog.feature.mypage
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun MypageRoute(
+    padding: PaddingValues,
+    onClick: () -> Unit,
+    onShowErrorSnackBar: (throwable: Throwable?) -> Unit
+) {
+    MypageScreen(
+        padding = padding,
+        onClick = onClick
+    )
+}
+
+@Composable
+private fun MypageScreen(
+    padding: PaddingValues,
+    onClick: () -> Unit
+) {
+    Text(text = "MYPAGE")
+}
