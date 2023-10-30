7ddc2f6e5247649128da3f80b92c15635db7527d
Minju Kim <mindori1019@gmail.com>
2023-10-30 13:55:24 +0900

    feature/main: MainNavigator 속 navigate 함수를 통한 화면 및 모듈 이동 구현

--- feature/management/src/main/java/com/kusitms/connectdog/feature/management/navigation/ManagementNavigation.kt
+++ feature/management/src/main/java/com/kusitms/connectdog/feature/management/navigation/ManagementNavigation.kt
@@ -1,5 +1,12 @@
 package com.kusitms.connectdog.feature.management.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+
+fun NavController.navigateManagement(navOptions: NavOptions) {
+    navigate(ManagementRoute.route, navOptions)
+}
+
 
 object ManagementRoute {
     const val route = "management"
--- feature/mypage/src/main/java/com/kusitms/connectdog/feature/mypage/MypageNavigation.kt
+++ feature/mypage/src/main/java/com/kusitms/connectdog/feature/mypage/MypageNavigation.kt
@@ -1,5 +1,13 @@
 package com.kusitms.connectdog.feature.mypage
 
+import androidx.navigation.NavController
+import androidx.navigation.NavOptions
+
+fun NavController.navigateMypage(navOptions: NavOptions) {
+    navigate(MypageRoute.route, navOptions)
+}
+
+
 object MypageRoute {
     const val route = "mypage"
 }
