202d2937d5720fe4103577da26c9143df1815156
Minju Kim <mindori1019@gmail.com>
2023-10-30 14:12:25 +0900

    fix/build: 라이브러리 호환성 문제로 target api 34로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.kusitms.connectdog"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.kusitms.connectdog"
         minSdk = 30
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
