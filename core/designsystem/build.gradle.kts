29eaa4f81387b6d2cd712897ae2cfe5548094d58
Minju Kim <mindori1019@gmail.com>
2023-11-06 16:01:19 +0900

    feature/designsystem : relay figma로 받아오기 위해 dependency 추가

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
     alias(libs.plugins.ktlint)
+    alias(libs.plugins.google.relay)
 }
 
 android {
@@ -39,6 +40,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
