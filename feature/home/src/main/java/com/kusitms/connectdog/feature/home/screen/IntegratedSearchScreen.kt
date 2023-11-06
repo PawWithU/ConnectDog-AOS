0ac755af51d6bba6624850bdf415d6f5311d0d0f
Minju Kim <mindori1019@gmail.com>
2023-11-06 16:55:50 +0900

    feature/home : 홈화면에 필요한 화면 생성

---
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/IntegratedSearchScreen.kt
@@ -0,0 +1,9 @@
+package com.kusitms.connectdog.feature.home.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun IntegratedSearchScreen(){
+    Text(text = "INTEGRATED SEARCH")
+}
---
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/SearchScreen.kt
@@ -0,0 +1,10 @@
+package com.kusitms.connectdog.feature.home.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+
+@Composable
+internal fun SearchScreen(){
+    Text(text = "SEARCH")
+}
