ec3b1b045b1f3c3f68b4862f84fc9f74d40ede89
Minju Kim <mindori1019@gmail.com>
2023-11-06 18:17:03 +0900

    feature/search: search 화면 뒤로 가기 topappbar + 기능 구현

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/SearchScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/SearchScreen.kt
@@ -1,13 +1,33 @@
 package com.kusitms.connectdog.feature.home.screen
 
 import android.util.Log
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
+import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
+import com.kusitms.connectdog.feature.home.R
 
 @Composable
 internal fun SearchScreen(
     onBackClick: () -> Unit
 ) {
-    Log.d("SearchScreen", "SearchScreen")
-    Text(text = "SEARCH")
+    Column {
+        TopAppBar {
+            onBackClick()
+        }
+        Text(text = "SEARCH")
+    }
+}
+
+@Composable
+private fun TopAppBar(
+    onBackClick: () -> Unit
+){
+    ConnectDogTopAppBar(
+        titleRes = R.string.search_app_bar_title,
+        navigationType = TopAppBarNavigationType.BACK,
+        navigationIconContentDescription = "Back",
+        onNavigationClick = { onBackClick() }
+    )
 }
