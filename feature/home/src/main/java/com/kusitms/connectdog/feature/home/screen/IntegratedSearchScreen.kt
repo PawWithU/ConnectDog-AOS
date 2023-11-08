7eff7f244e085db3895244184da0e11828dc6bbf
Minju Kim <mindori1019@gmail.com>
2023-11-09 02:59:15 +0900

    update/search: 와프 변경에 따라 필요없어진 파일 삭제

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/IntegratedSearchScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.kusitms.connectdog.feature.home.screen
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-internal fun IntegratedSearchScreen() {
-    Text(text = "INTEGRATED SEARCH")
-}
