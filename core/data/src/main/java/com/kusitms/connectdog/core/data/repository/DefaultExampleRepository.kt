841d35c66a9604fb1b2b83a2ca72c818f497d5fc
Seunggu Kang <alex9366@naver.com>
2023-11-09 02:32:11 +0900

    update: ktlint 포맷팅 적용

--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/DefaultExampleRepository.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/DefaultExampleRepository.kt
@@ -6,11 +6,11 @@ import com.kusitms.connectdog.core.model.Example
 import javax.inject.Inject
 
 internal class DefaultExampleRepository
-    @Inject
-    constructor(
-        private val api: ApiService,
-    ) : ExampleRepository {
-        override suspend fun getExample(): List<Example> {
-            return api.getExample().map { it.toData() }
-        }
+@Inject
+constructor(
+    private val api: ApiService
+) : ExampleRepository {
+    override suspend fun getExample(): List<Example> {
+        return api.getExample().map { it.toData() }
     }
+}
