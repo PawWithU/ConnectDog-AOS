0e24dcc0f1ef156111f79c17ac6349e13fd47600
Minju Kim <mindori1019@gmail.com>
2023-11-09 13:18:20 +0900

    feature/home: 이동봉사 공고 uistate 생성

---
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/state/AnnouncementUiState.kt
@@ -0,0 +1,11 @@
+package com.kusitms.connectdog.feature.home.state
+
+import com.kusitms.connectdog.core.model.Announcement
+
+sealed interface AnnouncementUiState {
+    object Loading: AnnouncementUiState
+    object Empty: AnnouncementUiState
+    data class Announcements(
+        val announcements: List<Announcement>
+    ): AnnouncementUiState
+}
