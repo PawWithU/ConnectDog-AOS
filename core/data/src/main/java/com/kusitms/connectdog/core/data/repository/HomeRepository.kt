cf94c970daec57c426bae1ca04b8405221f4ceb2
Minju Kim <mindori1019@gmail.com>
2023-11-09 19:47:55 +0900

    feature/home: review api 통신 함수 구현

--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/HomeRepository.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/HomeRepository.kt
@@ -1,7 +1,10 @@
 package com.kusitms.connectdog.core.data.repository
 
 import com.kusitms.connectdog.core.model.Announcement
+import com.kusitms.connectdog.core.model.Review
 
 interface HomeRepository {
     suspend fun getAnnouncementList(): List<Announcement>
+
+    suspend fun getReviewList(): List<Review>
 }
--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/HomeRepositoryImpl.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/HomeRepositoryImpl.kt
@@ -2,12 +2,17 @@ package com.kusitms.connectdog.core.data.repository
 
 import com.kusitms.connectdog.core.data.api.ApiService
 import com.kusitms.connectdog.core.model.Announcement
+import com.kusitms.connectdog.core.model.Review
 import javax.inject.Inject
 
 internal class HomeRepositoryImpl @Inject constructor(
     private val api: ApiService
 ) : HomeRepository {
     override suspend fun getAnnouncementList(): List<Announcement> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getReviewList(): List<Review> {
         TODO("Not yet implemented")
     }
 }
