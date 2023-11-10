ca501c5980384dab4964d029df821f9ef23d5af5
Seunggu Kang <alex9366@naver.com>
2023-11-10 23:29:43 +0900

    chore: compose-runtime-livedat 라이브러리 추가

--- feature/login/build.gradle.kts
+++ feature/login/build.gradle.kts
@@ -68,6 +68,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.runtime.livedata)
 
     implementation(libs.kotlinx.collection.imuutable)
 
