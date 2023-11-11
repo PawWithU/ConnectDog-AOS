ef1ac866ea494815a4cc5010a4f6f24bdf1311b7
Minju Kim <mindori1019@gmail.com>
2023-11-11 12:58:13 +0900

    feature/designsystem: 공고목록 UI Component 구현 완료

--- core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Content.kt
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Content.kt
@@ -1,5 +1,6 @@
 package com.kusitms.connectdog.core.designsystem.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,6 +10,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,6 +18,7 @@ import androidx.compose.ui.unit.dp
 import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
 import com.kusitms.connectdog.core.designsystem.theme.Gray2
 import com.kusitms.connectdog.core.designsystem.theme.Gray3
+import com.kusitms.connectdog.core.designsystem.theme.Gray6
 
 @Composable
 fun Content(
@@ -26,12 +29,28 @@ fun Content(
     titleRes3: Int,
     t1Value: String,
     t2Value: String,
-    t3Value: String
+    t3Value: String,
+    isValid: Boolean = true
 ) {
-    Column(modifier = modifier) {
-        TitleValue(title = stringResource(id = titleRes1), value = t1Value, style = textStyle)
-        TitleValue(title = stringResource(id = titleRes2), value = t2Value, style = textStyle)
-        TitleValue(title = stringResource(id = titleRes3), value = t3Value, style = textStyle)
+    Column(modifier = modifier, verticalArrangement = Arrangement.spacedBy(6.dp)) {
+        TitleValue(
+            title = stringResource(id = titleRes1),
+            value = t1Value,
+            style = textStyle,
+            isValid = isValid
+        )
+        TitleValue(
+            title = stringResource(id = titleRes2),
+            value = t2Value,
+            style = textStyle,
+            isValid = isValid
+        )
+        TitleValue(
+            title = stringResource(id = titleRes3),
+            value = t3Value,
+            style = textStyle,
+            isValid = isValid
+        )
     }
 }
 
@@ -39,27 +58,28 @@ fun Content(
 fun TitleValue(
     title: String,
     value: String,
-    style: TextStyle
+    style: TextStyle,
+    isValid: Boolean = true
 ) {
     Row {
         Text(
             text = title,
             style = style,
-            color = Gray3,
+            color = if (isValid) Gray3 else Gray6,
             modifier = Modifier.defaultMinSize(minWidth = 35.dp)
         )
         Spacer(modifier = Modifier.width(12.dp))
         Text(
             text = value,
             style = style,
-            color = Gray2
+            color = if (isValid) Gray2 else Gray6
         )
     }
 }
 
 @Preview
 @Composable
-fun ContentPreview() {
+private fun ContentPreview() {
     ConnectDogTheme {
         TitleValue(title = "일정", value = "23.10.19(목)", style = MaterialTheme.typography.labelLarge)
     }
---
+++ core/model/src/main/java/com/kusitms/connectdog/core/model/Applicant.kt
@@ -0,0 +1,9 @@
+package com.kusitms.connectdog.core.model
+
+data class Applicant(
+    val contentUrl: String,
+    val dogName: String,
+    val date: String,
+    val location: String,
+    val volunteer: String,
+)
