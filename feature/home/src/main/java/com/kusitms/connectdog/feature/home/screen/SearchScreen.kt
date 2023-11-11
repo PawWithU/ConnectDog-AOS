63399ef1881e2f2edbff199a2a1d824128e35b3e
Minju Kim <mindori1019@gmail.com>
2023-11-11 16:03:38 +0900

    feature/search: search bar 구현

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/SearchScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/SearchScreen.kt
@@ -1,10 +1,31 @@
 package com.kusitms.connectdog.feature.home.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
 import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
+import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
+import com.kusitms.connectdog.core.designsystem.theme.Gray3
+import com.kusitms.connectdog.core.designsystem.theme.Gray4
+import com.kusitms.connectdog.core.designsystem.theme.Gray7
 import com.kusitms.connectdog.feature.home.R
 
 @Composable
@@ -15,7 +36,12 @@ internal fun SearchScreen(
         TopAppBar {
             onBackClick()
         }
-        Text(text = "SEARCH")
+        SearchBar(
+            modifier = Modifier
+                .padding(horizontal = 13.dp, vertical = 6.dp)
+                .fillMaxWidth()
+        ) { //todo 검색 popup
+        }
     }
 }
 
@@ -29,4 +55,38 @@ private fun TopAppBar(
         navigationIconContentDescription = "Back",
         onNavigationClick = { onBackClick() }
     )
+}
+
+@Composable
+private fun SearchBar(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier = modifier
+            .background(color = Gray7, shape = RoundedCornerShape(6.dp))
+            .padding(horizontal = 12.dp, vertical = 15.dp)
+            .clickable { onClick() }
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "search icon",
+            tint = Gray3
+        )
+        Spacer(modifier = Modifier.width(12.dp))
+        Text(
+            text = stringResource(id = R.string.search_where_to_move),
+            style = MaterialTheme.typography.titleSmall,
+            fontSize = 14.sp,
+            color = Gray4
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+    ConnectDogTheme {
+        SearchScreen {}
+    }
 }
