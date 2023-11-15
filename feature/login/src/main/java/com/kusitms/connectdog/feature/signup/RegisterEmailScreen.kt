33a9c9e3c15c8e2977f6bad07a6aff366e6dc21e
Seunggu Kang <alex9366@naver.com>
2023-11-16 00:51:08 +0900

    update: PetOrange를 MaterialTheme에서 가져오도록 수정

--- feature/login/src/main/java/com/kusitms/connectdog/feature/signup/RegisterEmailScreen.kt
+++ feature/login/src/main/java/com/kusitms/connectdog/feature/signup/RegisterEmailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -25,7 +26,6 @@ import com.kusitms.connectdog.core.designsystem.R
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTextFieldWithButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
 import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
-import com.kusitms.connectdog.core.designsystem.theme.PetOrange
 import com.kusitms.connectdog.feature.login.NormalButton
 
 @Composable
@@ -89,7 +89,7 @@ fun RegisterEmailScreen(
         }
         NormalButton(
             content = "다음",
-            color = PetOrange,
+            color = MaterialTheme.colorScheme.primary,
             onClick = { navigator.navigate("registerPassword") },
             modifier =
             Modifier
