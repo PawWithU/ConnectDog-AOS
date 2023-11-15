34707c1c3b3e96a7debcee01b344322c9c5f117f
Minju Kim <mindori1019@gmail.com>
2023-11-15 17:18:58 +0900

    feat/designsystem: ExpandableCard 구현

--- core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
@@ -1,10 +1,18 @@
 package com.kusitms.connectdog.core.designsystem.component
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -12,14 +20,30 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ConnectDogCard(
     modifier: Modifier = Modifier,
-    color: Color = MaterialTheme.colorScheme.surface,
+    borderColor: Color = MaterialTheme.colorScheme.outline,
     content: @Composable () -> Unit
 ) {
     Surface(
-        modifier = modifier.fillMaxWidth(),
-        color = color,
-        shape = RoundedCornerShape(32.dp),
+        modifier = modifier.fillMaxWidth().padding(20.dp),
+        border = BorderStroke(width = 1.dp, color = borderColor),
+        color = MaterialTheme.colorScheme.surface,
+        shape = RoundedCornerShape(12.dp),
         shadowElevation = 2.dp,
         content = content
     )
+}
+
+@Composable
+fun ConnectDogExpandableCard(
+    modifier: Modifier = Modifier,
+    defaultContent: @Composable () -> Unit,
+    expandedContent: @Composable () -> Unit
+) {
+    var expanded by remember { mutableStateOf(true) }
+    ConnectDogCard(
+        modifier = modifier.fillMaxWidth().clickable { expanded = !expanded },
+    ){
+        if (expanded) expandedContent()
+        else defaultContent()
+    }
 }
