debdce138adc88b90a48da8c0d34fd6b8da933a6
Seunggu Kang <alex9366@naver.com>
2023-11-18 21:00:43 +0900

    feat: 정보를 표시하기 위한 Card 추가

--- core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
@@ -1,13 +1,25 @@
 package com.kusitms.connectdog.core.designsystem.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kusitms.connectdog.core.designsystem.theme.Gray3
+import com.kusitms.connectdog.core.designsystem.theme.Gray7
 
 @Composable
 fun ConnectDogCard(
@@ -22,4 +34,34 @@ fun ConnectDogCard(
         shadowElevation = 2.dp,
         content = content
     )
+}
+
+@Composable
+fun ConnectDogInformationCard(
+    title: String,
+    content: String
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(4.dp))
+            .background(Gray7)
+            .padding(all = 10.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = title,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = content,
+                fontSize = 14.sp,
+                color = Gray3
+            )
+        }
+    }
 }
