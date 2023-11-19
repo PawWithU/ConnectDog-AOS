051b140b6f2152d387e4238e80e7c6137e346592
Minju Kim <mindori1019@gmail.com>
2023-11-19 23:42:52 +0900

    feat/network: 필요없는 예시 example* 삭제

--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/ExampleRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.kusitms.connectdog.core.data.repository
-
-import com.kusitms.connectdog.core.model.Example
-
-interface ExampleRepository {
-    suspend fun getExample(): List<Example>
-}
