ea31eeb34435e93f9c78c98c07d0571cde8b5614
Seunggu Kang <alex9366@naver.com>
2023-11-20 01:51:45 +0900

    update: 수정된 경로 반영

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/CompleteApplyScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/CompleteApplyScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kusitms.connectdog.core.designsystem.R
+import com.kusitms.connectdog.core.designsystem.component.NormalButton
 import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
 import com.kusitms.connectdog.core.designsystem.theme.Gray2
 
