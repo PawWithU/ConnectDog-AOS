2712610f77ed0e7005d3a1cbdd6684e74607b987
Seunggu Kang <alex9366@naver.com>
2023-11-20 01:52:32 +0900

    update: 새로 구현한 화면으로 이동할 수 있도록 구현

--- feature/main/src/main/java/com/kusitms/connectdog/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/kusitms/connectdog/feature/main/MainScreen.kt
@@ -55,10 +55,21 @@ internal fun MainScreen(navigator: MainNavigator = rememberMainNavigator()) {
                         onNavigateToApply = { navigator.navigateApply() },
                         onNavigateToComplete = { navigator.navigateComplete() },
                         onNavigateToIntermediatorProfile = { navigator.navigateIntermediatorProfile()},
+                        onNavigateToNotification = { navigator.navigateNotification() },
                         onShowErrorSnackBar = {}
                     )
                     managementNavGraph(padding = it, onClick = {}, onShowErrorSnackbar = {})
-                    mypageNavGraph(padding = it, onClick = {}, onShowErrorSnackbar = {})
+                    mypageNavGraph(
+                        padding = it,
+                        onClick = {},
+                        onBackClick = navigator::popBackStackIfNotHome,
+                        onEditProfileClick = { navigator.navigateEditProfile() },
+                        onManageAccountClick = { navigator.navigateManageAccount() },
+                        onNotificationClick = { navigator.navigateNotification() },
+                        onSettingClick = { navigator.navigateSetting() },
+                        onBadgeClick = { navigator.navigateBadge() },
+                        onShowErrorSnackbar = {}
+                    )
                 }
             }
         },
