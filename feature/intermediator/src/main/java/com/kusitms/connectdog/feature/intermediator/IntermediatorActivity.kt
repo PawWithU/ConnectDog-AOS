8f1a94a5bff1fdef68f9aa17871b9c90dd6a1217
Seunggu Kang <alex9366@naver.com>
2023-11-20 12:59:09 +0900

    feat: Navigator 구현

---
+++ feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/IntermediatorActivity.kt
@@ -0,0 +1,31 @@
+package com.kusitms.connectdog.feature.intermediator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
+import com.kusitms.connectdog.feature.intermediator.screen.IntermediatorHomeScreen
+
+class IntermediatorActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        actionBar?.hide()
+        
+        setContent {
+            val navigator = rememberNavController()
+            ConnectDogTheme {
+                NavHost(navigator, startDestination = "home") {
+                    composable("home") {
+                        IntermediatorHomeScreen(
+                            onNotificationClick = { /*TODO*/ },
+                            onSettingClick = {}
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
