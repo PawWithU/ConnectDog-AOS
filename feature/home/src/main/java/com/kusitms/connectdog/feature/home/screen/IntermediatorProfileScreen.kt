7adac753086af54ad263ef28743d39389ab078d6
Seunggu Kang <alex9366@naver.com>
2023-11-20 12:56:24 +0900

    feat: 중개자 프로필 ui 구현

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/IntermediatorProfileScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/IntermediatorProfileScreen.kt
@@ -33,6 +33,7 @@ import com.google.accompanist.pager.rememberPagerState
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogInformationCard
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogOutlinedButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
+import com.kusitms.connectdog.core.designsystem.component.DetailInfo
 import com.kusitms.connectdog.core.designsystem.component.NetworkImage
 import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
 import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
