49157c267dcac21a36224753d443fc7ee039bc47
Seunggu Kang <alex9366@naver.com>
2023-11-23 00:13:39 +0900

    feat: 마이페이지 api 연결에 필요한 함수 추가

--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/MyPageRepository.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/MyPageRepository.kt
@@ -1,7 +1,13 @@
 package com.kusitms.connectdog.core.data.repository
 
 import com.kusitms.connectdog.core.data.api.model.MyInfoResponseItem
+import com.kusitms.connectdog.core.data.api.model.volunteer.BadgeResponse
+import com.kusitms.connectdog.core.data.api.model.volunteer.BookmarkResponseItem
+import com.kusitms.connectdog.core.data.api.model.volunteer.UserInfoResponse
 
 interface MyPageRepository {
     suspend fun getMyInfo(): MyInfoResponseItem
+    suspend fun getUserInfo(): UserInfoResponse
+    suspend fun getBadge(): List<BadgeResponse>
+    suspend fun getBookmarkData(): List<BookmarkResponseItem>
 }
