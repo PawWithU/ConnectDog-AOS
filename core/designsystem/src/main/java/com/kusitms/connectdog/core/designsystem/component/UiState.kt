8ceb7b743cbb6cfa2842c32e5a38599ca6611644
Minju Kim <mindori1019@gmail.com>
2023-11-23 01:02:19 +0900

    update/style: empty ui 예쁘게 수정

--- core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/UiState.kt
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/UiState.kt
@@ -2,10 +2,12 @@ package com.kusitms.connectdog.core.designsystem.component
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -33,11 +35,11 @@ fun Empty(
     @StringRes titleRes: Int,
     @StringRes descriptionRes: Int,
 ) {
-    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(painter = painterResource(id = R.drawable.img_dog_sad), contentDescription = "empty")
+    Column(modifier = modifier.fillMaxSize().offset(y = (-60).dp), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+        Image(painter = painterResource(id = R.drawable.img_dog_sad), contentDescription = "empty", modifier.size(width = 65.dp, height = 139.dp))
         Spacer(modifier = Modifier.size(20.dp))
         Text(text = stringResource(id = titleRes), style = MaterialTheme.typography.bodyMedium, color = Gray2, fontWeight = FontWeight.Medium)
         Spacer(modifier = Modifier.size(6.dp))
-        Text(text = stringResource(id = descriptionRes), style = MaterialTheme.typography.bodyLarge, color = Gray2)
+        Text(text = stringResource(id = descriptionRes), style = MaterialTheme.typography.labelLarge, color = Gray2)
     }
 }
