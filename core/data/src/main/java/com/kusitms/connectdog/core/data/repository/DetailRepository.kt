7c0e4cc9d2d89a0896473257f3f6e975aa1f98ae
Seunggu Kang <alex9366@naver.com>
2023-11-23 23:32:28 +0900

    update: 중개자 프로필 후기 조회 api 연결

--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/DetailRepository.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/DetailRepository.kt
@@ -1,13 +1,16 @@
 package com.kusitms.connectdog.core.data.repository
 
+import com.kusitms.connectdog.core.data.api.model.ReviewResponseItem
 import com.kusitms.connectdog.core.data.api.model.intermediator.IntermediatorInfoResponseItem
 import com.kusitms.connectdog.core.data.api.model.volunteer.BookmarkResponseItem
 import com.kusitms.connectdog.core.data.api.model.volunteer.NoticeDetailResponseItem
+import com.kusitms.connectdog.core.model.Review
 
 interface DetailRepository {
     suspend fun getNoticeDetail(postId: Long): NoticeDetailResponseItem
     suspend fun getIntermediatorInfo(intermediaryId: Long): IntermediatorInfoResponseItem
-    suspend fun getIntermediatorReview(intermediaryId: Long): List<BookmarkResponseItem>
+    suspend fun getIntermediatorNotice(intermediaryId: Long): List<BookmarkResponseItem>
+    suspend fun getIntermediatorReview(page: Int? = 0, size: Int? = 0, intermediaryId: Long): List<Review>
     suspend fun postBookmark(postId: Long)
     suspend fun deleteBookmark(postId: Long)
 }
--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/DetailRepositoryImpl.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/DetailRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.kusitms.connectdog.core.data.repository
 
 import com.kusitms.connectdog.core.data.api.ApiService
+import com.kusitms.connectdog.core.data.api.model.ReviewResponseItem
 import com.kusitms.connectdog.core.data.api.model.intermediator.IntermediatorInfoResponseItem
 import com.kusitms.connectdog.core.data.api.model.volunteer.BookmarkResponseItem
 import com.kusitms.connectdog.core.data.api.model.volunteer.NoticeDetailResponseItem
+import com.kusitms.connectdog.core.data.mapper.toData
+import com.kusitms.connectdog.core.model.Review
 import javax.inject.Inject
 
 internal class DetailRepositoryImpl @Inject constructor(
@@ -17,8 +20,16 @@ internal class DetailRepositoryImpl @Inject constructor(
         return api.getIntermediatorInfo(intermediaryId)
     }
 
-    override suspend fun getIntermediatorReview(intermediaryId: Long): List<BookmarkResponseItem> {
-        return api.getIntermediatorReview(intermediaryId)
+    override suspend fun getIntermediatorNotice(intermediaryId: Long): List<BookmarkResponseItem> {
+        return api.getIntermediatorNotice(intermediaryId)
+    }
+
+    override suspend fun getIntermediatorReview(
+        page: Int?,
+        size: Int?,
+        intermediaryId: Long
+    ): List<Review> {
+        return api.getIntermediatorReview(intermediaryId,page ?: 0, size ?: 0).map { it.toData() }
     }
 
     override suspend fun postBookmark(postId: Long) {
