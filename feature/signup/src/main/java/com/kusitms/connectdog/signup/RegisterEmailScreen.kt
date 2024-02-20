cd562771147f370c7cb7524262d779a6f7a18b5f
Seunggu Kang <alex9366@naver.com>
2024-02-20 23:46:30 +0900

    fix: 사용자의 유형에 따라 AppBar의 title을 다르게 설정하도록 수정 및 사용자 유형을 매개변수로 추가

--- feature/signup/src/main/java/com/kusitms/connectdog/signup/RegisterEmailScreen.kt
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/RegisterEmailScreen.kt
@@ -1,69 +1,69 @@
-package com.kusitms.connectdog.feature.signup
+package com.kusitms.connectdog.signup
 
-import androidx.compose.foundation.background
+import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import com.kusitms.connectdog.core.designsystem.R
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogNormalButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTextFieldWithButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
 import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
+import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
+import com.kusitms.connectdog.core.util.Type
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun RegisterEmailScreen(
-    navigator: NavController
+    onBackClick: () -> Unit,
+    type: Type,
+    onNavigateToRegisterPassword: (Type) -> Unit
 ) {
     val focusManager = LocalFocusManager.current
     val interactionSource = remember { MutableInteractionSource() }
-    Box(
-        modifier =
-        Modifier
-            .fillMaxSize()
-            .background(Color.White)
-            .padding(bottom = 32.dp)
-            .clickable(
-                onClick = { focusManager.clearFocus() },
-                indication = null,
-                interactionSource = interactionSource
-            )
-    ) {
-        Column(
-            modifier =
-            Modifier
-                .align(Alignment.TopCenter)
-                .background(Color.White)
-        ) {
+
+    Scaffold(
+        topBar = {
             ConnectDogTopAppBar(
-                titleRes = R.string.volunteer_signup,
+                titleRes = when (type) {
+                    Type.SOCIAL_VOLUNTEER -> R.string.volunteer_signup
+                    Type.NORMAL_VOLUNTEER -> R.string.volunteer_signup
+                    Type.INTERMEDIATOR -> R.string.intermediator_signup
+                },
                 navigationType = TopAppBarNavigationType.BACK,
                 navigationIconContentDescription = "Navigation icon",
-                onNavigationClick = { navigator.popBackStack() }
+                onNavigationClick = onBackClick
             )
-            Spacer(modifier = Modifier.height(37.dp))
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 20.dp)
+                .clickable(
+                    onClick = { focusManager.clearFocus() },
+                    indication = null,
+                    interactionSource = interactionSource
+                )
+        ) {
+            Spacer(modifier = Modifier.height(80.dp))
             Text(
                 text = "로그인에 사용할\n이메일을 입력해주세요",
                 fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(start = 20.dp)
+                fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(40.dp))
             ConnectDogTextFieldWithButton(
@@ -75,7 +75,6 @@ fun RegisterEmailScreen(
                 padding = 5
             )
             Spacer(modifier = Modifier.height(12.dp))
-
             ConnectDogTextFieldWithButton(
                 width = 62,
                 height = 27,
@@ -84,17 +83,24 @@ fun RegisterEmailScreen(
                 buttonLabel = "인증 확인",
                 padding = 5
             )
+            Spacer(modifier = Modifier.weight(1f))
+            ConnectDogNormalButton(
+                content = "다음",
+                onClick = { onNavigateToRegisterPassword(Type.NORMAL_VOLUNTEER) },
+                modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+            )
+            Spacer(modifier = Modifier.height(32.dp))
         }
-        ConnectDogNormalButton(
-            content = "다음",
-            color = MaterialTheme.colorScheme.primary,
-            onClick = { navigator.navigate("registerPassword") },
-            modifier =
-            Modifier
-                .fillMaxWidth()
-                .height(56.dp)
-                .padding(horizontal = 20.dp)
-                .align(Alignment.BottomCenter)
-        )
+    }
+}
+
+@Preview
+@Composable
+private fun test() {
+    ConnectDogTheme {
+        RegisterEmailScreen(onBackClick = {}, type = Type.NORMAL_VOLUNTEER, onNavigateToRegisterPassword = {})
     }
 }
