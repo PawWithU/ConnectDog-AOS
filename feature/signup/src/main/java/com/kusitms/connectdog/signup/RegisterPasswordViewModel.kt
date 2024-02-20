7aa638fd4b5e9f0d6261e91b3f4f822b2708ca2e
Seunggu Kang <alex9366@naver.com>
2024-02-21 03:51:37 +0900

    refactor: textField에 필요한 mutableState를 viewModel로 이전

--- feature/signup/src/main/java/com/kusitms/connectdog/signup/RegisterPasswordViewModel.kt
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/RegisterPasswordViewModel.kt
@@ -2,10 +2,13 @@ package com.kusitms.connectdog.signup
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
 
-class RegisterPasswordViewModel : ViewModel() {
+@HiltViewModel
+class RegisterPasswordViewModel @Inject constructor() : ViewModel() {
     private var _password = mutableStateOf("")
     val password: String
         get() = _password.value
