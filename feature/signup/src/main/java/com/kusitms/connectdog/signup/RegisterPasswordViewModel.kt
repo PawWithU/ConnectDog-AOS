002774e75fe80b0d8ffc312fe1efc29f7ae2d280
Seunggu Kang <alex9366@naver.com>
2024-02-21 02:35:09 +0900

    feat: 비밀번호 양식 및 재입력 비밀번호 검증 구현

---
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/RegisterPasswordViewModel.kt
@@ -0,0 +1,43 @@
+package com.kusitms.connectdog.signup
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class RegisterPasswordViewModel : ViewModel() {
+    private var _password = mutableStateOf("")
+    val password: String
+        get() = _password.value
+
+    private var _confirmPassword = mutableStateOf("")
+    val confirmPassword: String
+        get() = _confirmPassword.value
+
+    private val _isValidPassword: MutableStateFlow<Boolean?> = MutableStateFlow(null)
+    val isValidPassword: StateFlow<Boolean?>
+        get() = _isValidPassword
+
+    private val _isValidConfirmPassword: MutableStateFlow<Boolean?> = MutableStateFlow(null)
+    val isValidConfirmPassword: StateFlow<Boolean?>
+        get() = _isValidConfirmPassword
+
+
+    fun updatePassword(password: String) {
+        _password.value = password
+    }
+
+    fun updateConfirmPassword(confirmPassword: String) {
+        _confirmPassword.value = confirmPassword
+    }
+
+    fun checkPasswordValidity(password: String) {
+        val pattern1 = "^[a-zA-Z0-9]{10,}$"
+        val pattern2 = "^[a-zA-Z0-9!@#$%^&*()_+}{\":?><,./;'\\[\\]]{8,}$"
+        _isValidPassword.value = !Regex(pattern1).matches(password) || !Regex(pattern2).matches(password)
+    }
+
+    fun checkConfirmPasswordValidity(confirmPassword: String) {
+        _isValidConfirmPassword.value = confirmPassword != _password.value
+    }
+}
