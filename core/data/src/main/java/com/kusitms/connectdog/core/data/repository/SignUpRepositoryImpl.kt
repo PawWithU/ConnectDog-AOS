bb62f43482fd1ce7d5acc79f31bfc45f09f5817e
Seunggu Kang <alex9366@naver.com>
2024-02-21 11:45:21 +0900

    feat: SignUpRepository 추가

---
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/SignUpRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package com.kusitms.connectdog.core.data.repository
+
+import com.kusitms.connectdog.core.data.api.ApiService
+import com.kusitms.connectdog.core.data.api.model.IsDuplicateNicknameResponse
+import com.kusitms.connectdog.core.data.api.model.volunteer.IsDuplicateNicknameBody
+import javax.inject.Inject
+
+internal class SignUpRepositoryImpl @Inject constructor(
+    private val api: ApiService
+): SignUpRepository {
+    override suspend fun postNickname(nickname: IsDuplicateNicknameBody): IsDuplicateNicknameResponse {
+        return api.postNickname(nickname)
+    }
+}
