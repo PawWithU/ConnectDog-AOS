d417754705dcd3500a40aa38131a70e37f5aac79
Seunggu Kang <alex9366@naver.com>
2024-02-21 11:44:32 +0900

    feat: 닉네임 중복 확인 api 연결

--- feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/VolunteerProfileViewModel.kt
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/VolunteerProfileViewModel.kt
@@ -1,16 +1,23 @@
 package com.kusitms.connectdog.signup.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kusitms.connectdog.core.data.api.model.volunteer.IsDuplicateNicknameBody
+import com.kusitms.connectdog.core.data.repository.SignUpRepository
 import com.kusitms.connectdog.core.util.getProfileImageId
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class VolunteerProfileViewModel @Inject constructor() : ViewModel() {
+class VolunteerProfileViewModel @Inject constructor(
+    private val signupRepository: SignUpRepository
+) : ViewModel() {
     private val _selectedImageIndex = MutableStateFlow(-1)
     val selectedImageIndex: StateFlow<Int>
         get() = _selectedImageIndex
@@ -24,7 +31,14 @@ class VolunteerProfileViewModel @Inject constructor() : ViewModel() {
         get() = _nickname.value
 
     fun updateNicknameAvailability(nickname: String) {
-        _isNicknameAvailable.value = nickname == "ssaaw"
+        val nicknameBody = IsDuplicateNicknameBody(nickname = nickname)
+        viewModelScope.launch {
+            try {
+                _isNicknameAvailable.value = signupRepository.postNickname(nicknameBody).isDuplicated
+            } catch (e: Exception) {
+                Log.d("error", e.message.toString())
+            }
+        }
     }
 
     fun updateProfileImageIndex(imageIndex: Int) {
