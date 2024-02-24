d206e86931616e80eb32ea34981fefc1732679a8
Seunggu Kang <alex9366@naver.com>
2024-02-25 01:27:38 +0900

    feat: 키보드가 활성화되면 높이를 측정하는 listener 구현

--- feature/main/src/main/java/com/kusitms/connectdog/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/kusitms/connectdog/feature/main/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.kusitms.connectdog.feature.main
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsAnimationCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
@@ -20,6 +28,8 @@ import java.util.concurrent.TimeUnit
 class MainActivity : ComponentActivity() {
     private lateinit var auth: FirebaseAuth
     private lateinit var verificationId: String
+    private var isKeyboardShowing = false
+    private var imeHeight by mutableIntStateOf(0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,21 +40,17 @@ class MainActivity : ComponentActivity() {
         val type = Mode.LOGIN
 
         installSplashScreen()
+        imeListener()
         setContent {
             val navigator: MainNavigator = rememberMainNavigator(type = type)
             ConnectDogTheme {
                 MainScreen(
                     navigator = navigator,
                     mode = type,
-                    sendVerificationCode = {
-                        sendVerificationCode("+82${it.substring(1)}")
-                    },
-                    verifyCode = {
-                        verifyCode(it)
-                    },
-                    finish = {
-                        finish()
-                    }
+                    imeHeight = imeHeight,
+                    sendVerificationCode = { sendVerificationCode("+82${it.substring(1)}") },
+                    verifyCode = { verifyCode(it) },
+                    finish = { finish() }
                 )
             }
         }
@@ -94,5 +100,39 @@ class MainActivity : ComponentActivity() {
                 }
             }
         )
+    }
+
+    private fun imeListener() {
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+        ViewCompat.setWindowInsetsAnimationCallback(
+            window.decorView.rootView,
+            object : WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_STOP) {
+                override fun onProgress(
+                    insets: WindowInsetsCompat,
+                    runningAnimations: MutableList<WindowInsetsAnimationCompat>
+                ): WindowInsetsCompat {
+                    val imeHeight = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom
+                    val sysBarInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+                    val updatedHeight =
+                        if (imeHeight - sysBarInsets.bottom < 0) 0 else imeHeight - sysBarInsets.bottom
+                    Log.d("saqa", updatedHeight.toString())
+                    return insets
+                }
+            }
+        )
+
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView.rootView) { view, windowInsets ->
+            val density = resources.displayMetrics.density
+            val imeHeight = (windowInsets.getInsets(WindowInsetsCompat.Type.ime()).bottom / density).toInt()
+            Log.d("tewq", imeHeight.toString())
+            if (imeHeight != 0) {
+                this@MainActivity.imeHeight = imeHeight - 20
+            } else {
+                this@MainActivity.imeHeight = 0
+            }
+
+            ViewCompat.onApplyWindowInsets(view, windowInsets)
+        }
     }
 }
