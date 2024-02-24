2f70c0cbd055ad0fb925e9298c5eaad5a65f373d
Seunggu Kang <alex9366@naver.com>
2024-02-24 16:31:42 +0900

    refactor: 변수명 수정

--- core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
+++ core/designsystem/src/main/java/com/kusitms/connectdog/core/designsystem/component/Card.kt
@@ -88,7 +88,7 @@ fun ConnectDogInformationCard(
 @Composable
 fun ConnectDogExpandableCard(
     modifier: Modifier = Modifier,
-    isExpended: Boolean = false,
+    isExpanded: Boolean = false,
     onClick: () -> Unit,
     defaultContent: @Composable () -> Unit,
     expandedContent: @Composable () -> Unit
@@ -99,7 +99,7 @@ fun ConnectDogExpandableCard(
             .wrapContentHeight()
             .clickable { onClick() }
     ) {
-        if (isExpended) {
+        if (isExpanded) {
             expandedContent()
         } else {
             defaultContent()
