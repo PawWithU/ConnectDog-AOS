47bac367b34a4ebf510a4b96e6cfe8e4bbd686de
Seunggu Kang <alex9366@naver.com>
2024-02-26 11:42:39 +0900

    refactor: 변수명 수정

--- core/util/src/main/java/com/kusitms/connectdog/core/util/Constants.kt
+++ core/util/src/main/java/com/kusitms/connectdog/core/util/Constants.kt
@@ -1,12 +1,12 @@
 package com.kusitms.connectdog.core.util
 
-enum class Mode {
+enum class AppMode {
     LOGIN,
     VOLUNTEER,
     INTERMEDIATOR
 }
 
-enum class Type {
+enum class UserType {
     SOCIAL_VOLUNTEER,
     NORMAL_VOLUNTEER,
     INTERMEDIATOR
