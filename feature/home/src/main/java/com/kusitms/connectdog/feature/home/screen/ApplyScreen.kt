9077009d80371f01fa29a128a377d4a99783967b
Seunggu Kang <alex9366@naver.com>
2024-02-28 21:40:45 +0900

    feat: 이름과 휴대폰 번호에 대한 기본 정보 불러오기 기능 구현

--- feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/ApplyScreen.kt
+++ feature/home/src/main/java/com/kusitms/connectdog/feature/home/screen/ApplyScreen.kt
@@ -3,26 +3,40 @@ package com.kusitms.connectdog.feature.home.screen
 import android.annotation.SuppressLint
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -32,7 +46,9 @@ import com.kusitms.connectdog.core.designsystem.component.ConnectDogNormalButton
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTextField
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogTopAppBar
 import com.kusitms.connectdog.core.designsystem.component.TopAppBarNavigationType
+import com.kusitms.connectdog.core.designsystem.theme.ConnectDogTheme
 import com.kusitms.connectdog.core.designsystem.theme.Gray2
+import com.kusitms.connectdog.core.designsystem.theme.Gray5
 import com.kusitms.connectdog.core.designsystem.theme.Orange20
 import com.kusitms.connectdog.feature.home.ApplyViewModel
 
@@ -42,7 +58,8 @@ fun ApplyScreen(
     onBackClick: () -> Unit = {},
     onClick: () -> Unit = {},
     postId: Long,
-    viewModel: ApplyViewModel = hiltViewModel()
+    imeHeight: Int,
+    viewModel: ApplyViewModel
 ) {
     Scaffold(
         topBar = {
@@ -54,7 +71,7 @@ fun ApplyScreen(
             )
         }
     ) {
-        Content(viewModel, postId, onClick)
+        Content(viewModel, postId, imeHeight, onClick)
     }
 }
 
@@ -62,15 +79,19 @@ fun ApplyScreen(
 private fun Content(
     viewModel: ApplyViewModel,
     postId: Long,
+    imeHeight: Int,
     onClick: () -> Unit
 ) {
     val focusManager = LocalFocusManager.current
     val interactionSource = remember { MutableInteractionSource() }
+    val isChecked by viewModel.isChecked.collectAsState()
+    val scrollState = rememberScrollState()
 
     Column(
         modifier = Modifier
-            .padding(start = 20.dp, end = 20.dp, top = 48.dp, bottom = 32.dp)
-            .verticalScroll(rememberScrollState())
+            .padding(start = 20.dp, end = 20.dp, top = 80.dp)
+            .verticalScroll(scrollState)
+            .fillMaxHeight()
             .clickable(
                 onClick = { focusManager.clearFocus() },
                 indication = null,
@@ -83,16 +104,10 @@ private fun Content(
             fontSize = 20.sp
         )
         Spacer(modifier = Modifier.height(20.dp))
-//        Row(verticalAlignment = Alignment.CenterVertically) {
-//            Checkbox(checked = false, onCheckedChange = null)
-//            Spacer(modifier = Modifier.width(4.dp))
-//            Text(
-//                text = "기본정보 불러오기",
-//                color = Gray2,
-//                fontWeight = FontWeight.Normal,
-//                fontSize = 14.sp
-//            )
-//        }
+        BasicInformation(isChecked) {
+            viewModel.updateIsChecked()
+        }
+        Spacer(modifier = Modifier.height(20.dp))
         Text(
             text = "성함을 입력해주세요",
             fontWeight = FontWeight.Bold,
@@ -101,12 +116,13 @@ private fun Content(
         Spacer(modifier = Modifier.height(12.dp))
         ConnectDogTextField(
             text = viewModel.name,
+            enabled = !isChecked,
             label = "이름",
             placeholder = "이름 입력",
             keyboardType = KeyboardType.Text,
             onTextChanged = { viewModel.updateName(it) }
         )
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(40.dp))
         Text(
             text = "휴대폰 번호를 입력해주세요",
             fontWeight = FontWeight.Bold,
@@ -114,6 +130,7 @@ private fun Content(
         )
         Spacer(modifier = Modifier.height(12.dp))
         ConnectDogTextField(
+            enabled = !isChecked,
             text = viewModel.phoneNumber,
             label = "휴대폰 번호",
             placeholder = "휴대폰 번호 입력",
@@ -136,7 +153,7 @@ private fun Content(
             keyboardType = KeyboardType.Text,
             onTextChanged = { viewModel.updateTransportation(it) }
         )
-        Spacer(modifier = Modifier.height(30.dp))
+        Spacer(modifier = Modifier.height(40.dp))
         Text(
             text = "봉사자의 한마디",
             fontWeight = FontWeight.Bold,
@@ -148,10 +165,9 @@ private fun Content(
             label = "한마디 입력",
             placeholder = "신청한 이유, 봉사 이력 등을 간단하게 작성해주세요!\n(10자 이상, 100자 이내로 입력해주세요)",
             keyboardType = KeyboardType.Text,
-            height = 129,
+            height = 160,
             onTextChanged = { viewModel.updateContent(it) }
         )
-
         ConnectDogNormalButton(
             content = "완료",
             onClick = {
@@ -167,12 +183,14 @@ private fun Content(
                 onClick()
             }
         )
+        Spacer(modifier = Modifier.height((imeHeight + 32).dp))
     }
 }
 
 @Composable
 fun NoticeCard() {
     Card(
+        shape = RoundedCornerShape(6.dp),
         colors = CardDefaults.cardColors(containerColor = Orange20),
         modifier = Modifier
             .fillMaxWidth()
@@ -185,6 +203,48 @@ fun NoticeCard() {
             modifier = Modifier
                 .fillMaxSize()
                 .wrapContentSize(Alignment.Center)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun BasicInformation(
+    isChecked: Boolean,
+    updateIsChecked: () -> Unit
+) {
+    Row(
+        modifier = Modifier.padding(horizontal = 0.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start
+    ) {
+        CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
+            Checkbox(
+                checked = isChecked,
+                colors = CheckboxDefaults.colors(
+                    uncheckedColor = Gray5
+                ),
+                onCheckedChange = { updateIsChecked() }
+            )
+        }
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = "기본정보 불러오기",
+            color = Gray2,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun ApplyScreenPreview() {
+    ConnectDogTheme {
+        ApplyScreen(
+            postId = 1,
+            imeHeight = 200,
+            viewModel = hiltViewModel()
         )
     }
 }
