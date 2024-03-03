3ca30240adc95b570c3ca51fe2f039e3311ed434
Seunggu Kang <alex9366@naver.com>
2024-03-04 01:18:13 +0900

    feat: 회원가입 관련 데이터를 공유하기 위한 viewModel 추가

---
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,59 @@
+package com.kusitms.connectdog.signup.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kusitms.connectdog.core.data.api.model.volunteer.NormalVolunteerSignUpBody
+import com.kusitms.connectdog.core.data.repository.SignUpRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val signupRepository: SignUpRepository
+) : ViewModel() {
+    private val _email = MutableStateFlow<String?>(null)
+    val email: StateFlow<String?> = _email
+
+    private val _password = MutableStateFlow<String?>(null)
+    val password: StateFlow<String?> = _password
+
+    private val _nickname = MutableStateFlow<String?>(null)
+    val nickname: StateFlow<String?> = _nickname
+
+    private val _profileImageId = MutableStateFlow<Int?>(null)
+    val profileImageId: StateFlow<Int?> = _profileImageId
+
+    fun updateEmail(email: String) {
+        _email.value = email
+    }
+
+    fun updatePassword(password: String) {
+        _password.value = password
+    }
+
+    fun updateNickname(nickname: String) {
+        _nickname.value = nickname
+    }
+
+    fun updateProfileImageId(profileImageId: Int) {
+        _profileImageId.value = profileImageId
+    }
+
+    fun postNormalVolunteerSignUp() {
+        val body = NormalVolunteerSignUpBody(
+            email = email.value!!,
+            nickname = nickname.value!!,
+            password = password.value!!,
+            profileImageNum = profileImageId.value!!
+        )
+
+        viewModelScope.launch {
+            val response = signupRepository.postNormalVolunteerSignUp(body)
+            Log.d("tesaq", response.toString())
+        }
+    }
+}
