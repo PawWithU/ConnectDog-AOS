ee4e496b59c0628d261313015dc62f8f82b410c9
Seunggu Kang <alex9366@naver.com>
2024-03-03 18:09:24 +0900

    feat: 이메일 인증 번호 발송 및 확인 기능 구현

--- feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/RegisterEmailViewModel.kt
+++ feature/signup/src/main/java/com/kusitms/connectdog/signup/viewmodel/RegisterEmailViewModel.kt
@@ -1,15 +1,22 @@
 package com.kusitms.connectdog.signup.viewmodel
 
+import android.util.Log
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kusitms.connectdog.core.data.api.model.volunteer.EmailCertificationBody
+import com.kusitms.connectdog.core.data.repository.SignUpRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class RegisterEmailViewModel @Inject constructor() : ViewModel() {
+class RegisterEmailViewModel @Inject constructor(
+    private val signUpRepository: SignUpRepository
+) : ViewModel() {
     private val _isEmailVerified: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isEmailVerified: StateFlow<Boolean>
         get() = _isEmailVerified
@@ -21,6 +28,8 @@ class RegisterEmailViewModel @Inject constructor() : ViewModel() {
     private val _email: MutableState<String> = mutableStateOf("")
     val email: String
         get() = _email.value
+
+    private var postNumber: String = ""
 
     private val _certificationNumber: MutableState<String> = mutableStateOf("")
     val certificationNumber: String
@@ -36,5 +45,22 @@ class RegisterEmailViewModel @Inject constructor() : ViewModel() {
 
     fun updateEmailValidity() {
         _isValidEmail.value = !android.util.Patterns.EMAIL_ADDRESS.matcher(_email.value).matches()
+    }
+
+    fun postEmail() {
+        viewModelScope.launch {
+            val body = EmailCertificationBody(_email.value)
+            try {
+                val response = signUpRepository.postEmail(body)
+                postNumber = response.authCode
+            } catch (e: Exception) {
+                Log.d("asdf", e.message.toString())
+            }
+        }
+    }
+
+    fun checkCertificationNumber() {
+        _isEmailVerified.value = _certificationNumber.value == postNumber
+        Log.d("asdfa", _isEmailVerified.value.toString())
     }
 }
