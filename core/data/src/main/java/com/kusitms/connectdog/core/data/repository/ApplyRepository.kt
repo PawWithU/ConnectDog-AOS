0c92e5de20b7ebf554739d8f3339d8f980f68bd7
Seunggu Kang <alex9366@naver.com>
2024-03-16 02:13:51 +0900

    feat: 추가 인증 정보를 post/get 하는 api 연결

--- core/data/src/main/java/com/kusitms/connectdog/core/data/repository/ApplyRepository.kt
+++ core/data/src/main/java/com/kusitms/connectdog/core/data/repository/ApplyRepository.kt
@@ -1,7 +1,10 @@
 package com.kusitms.connectdog.core.data.repository
 
+import com.kusitms.connectdog.core.data.api.model.AdditionalAuthBody
 import com.kusitms.connectdog.core.data.api.model.volunteer.ApplyBody
 
 interface ApplyRepository {
     suspend fun postApplyVolunteer(postId: Long, applyBody: ApplyBody)
+    suspend fun postAdditionalAuth(additionalAuthBody: AdditionalAuthBody)
+    suspend fun getAdditionalAuth(): AdditionalAuthBody
 }
