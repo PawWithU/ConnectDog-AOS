07ad4907ee969f86939c45a02f529afe8d859c69
Seunggu Kang <alex9366@naver.com>
2024-03-22 23:18:04 +0900

    update: 뱃지 데이터를 초기화 하는 로직 추가

--- feature/mypage/src/main/java/com/kusitms/connectdog/feature/mypage/screen/BadgeScreen.kt
+++ feature/mypage/src/main/java/com/kusitms/connectdog/feature/mypage/screen/BadgeScreen.kt
@@ -24,9 +24,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -79,7 +79,10 @@ fun BadgeScreen(
 ) {
     val showBottomSheet by viewModel.showBottomSheet.observeAsState(initial = false)
     val sheetState = rememberModalBottomSheetState()
-    val scope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+        viewModel.fetchBadge()
+    }
 
     Scaffold(
         topBar = {
