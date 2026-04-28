9ba2a8450c7f7ccc6871d92195c8fa79a4687012
Seunggu Kang <alex9366@naver.com>
2026-04-28 22:19:03 +0900

    fix: 불필요한 코드 제거

--- feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/screen/CompleteCreateScreen.kt
+++ feature/intermediator/src/main/java/com/kusitms/connectdog/feature/intermediator/screen/CompleteCreateScreen.kt
@@ -1,45 +1,36 @@
 package com.kusitms.connectdog.feature.intermediator.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layout
-import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.kusitms.connectdog.core.designsystem.R as DR
 import com.kusitms.connectdog.core.designsystem.component.ConnectDogNormalButton
 import com.kusitms.connectdog.core.designsystem.theme.Gray100
 import com.kusitms.connectdog.core.designsystem.theme.Gray60
-import com.kusitms.connectdog.core.designsystem.theme.Gray7
 import com.kusitms.connectdog.feature.intermediator.R
+import com.kusitms.connectdog.core.designsystem.R as DR
 
 @Composable
 fun CompleteCreateScreen(
     navigateToHome: () -> Unit
 ) {
-    val focusManager = LocalFocusManager.current
-    val interactionSource = remember { MutableInteractionSource() }
-
     Column(
         modifier = Modifier
             .fillMaxSize()
